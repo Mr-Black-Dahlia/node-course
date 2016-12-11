d236c1ceccb0d1cd649c4c845193b3a2d2a3a693
Matthew Ruffino <Matthew Ruffino>
2016-12-11 01:48:15 -0500

    added new projects page

--- server.js
+++ server.js
@@ -47,6 +47,12 @@ app.get('/about', (req, res) => {
   });
 });
 
+app.get('/projects', (req, res) => {
+  res.render('projects.hbs', {
+    pageTitle: 'Course Projects Page'
+  });
+});
+
 app.get('/bad', (req, res) => {
   res.send({
     errorMessage: 'Not Found'
